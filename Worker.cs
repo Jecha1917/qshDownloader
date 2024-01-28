de737225dd87c3d8c799498ecdeccaa5a3db5c93
Jecha <jecha666@gmail.com>
2024-01-28 21:33:15 +0300

    parallel download from servers

--- Worker.cs
+++ Worker.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 using System.Net.Http;
+using System.Collections.Concurrent;
 
 namespace qshDownloader
 {
@@ -26,8 +27,8 @@ namespace qshDownloader
 
 		/// <summary>
 		/// Match
-		/// Group[1] = relative url to file
-		/// Group[2] = file name
+		/// Group[1] = relative url to fileMatch
+		/// Group[2] = fileMatch name
 		/// </summary>
 		private readonly Regex _parseFileUrls;
 
@@ -83,9 +84,9 @@ namespace qshDownloader
 				DateTime startTime = DateTime.Now;
 				_logger.LogInformation("Downloader started at: {time}", DateTimeOffset.Now);
 
-				foreach (var urlServer in _config.ServerUrls)
+				await Task.WhenAll(_config.ServerUrls.Select(async urlServer =>
 				{
-                    IEnumerable<string> availableDays = await _folderEnumerators.First().Enumerate(new Uri(urlServer), CancellationToken.None);
+					IEnumerable<string> availableDays = await _folderEnumerators.First().Enumerate(new Uri(urlServer), CancellationToken.None);
 
 					_logger.LogInformation("Server: {string}", urlServer);
 					var url = new Uri(urlServer);
@@ -102,13 +103,13 @@ namespace qshDownloader
 
 						await Task.WhenAll(dirs.Select(async dir =>
 						{
-                            var files = await Task.Run(() =>
+							var files = await Task.Run(() =>
 								Directory.EnumerateFiles($"{_config.HistoryPath}\\{dir}"));
 
-                            _cache.Set(dir, files.ToList());
-                            files.ToList().ForEach(file =>
+							_cache.Set(dir, files.ToList());
+							files.ToList().ForEach(file =>
 								_cache.Set(file, FileDownloadStatusType.Success));
-                        }));
+						}));
 					}
 
 					if (availableDays.Count() > 0)
@@ -126,55 +127,68 @@ namespace qshDownloader
 								Directory.Delete(dayPathTmp, true);
 
 							Directory.CreateDirectory(dayPathTmp);
-							List<string> files = new List<string>();
+							var files = new ConcurrentBag<string>();
+
 
 							using var client = new HttpClient();
-							var data = await client.GetStringAsync(new Uri(baseUri: url, day), stoppingToken);
-                            var listFiles = _parseFileUrls.Matches(data);
+							var data = await client.GetStringAsync(new Uri($"{url}/{day}"), stoppingToken);
+							var listFiles = _parseFileUrls.Matches(data);
 
-							await Task.WhenAll(listFiles.Select(async k =>
+							await Task.WhenAll(listFiles.Select(async fileMatch =>
 							{
-                                string fileName = k.Groups[2].Value;
-                                for (int i = 0; i < _retryCount; i++)
-                                    try
-                                    {
-                                        var response = await client.GetAsync(new Uri(url, k.Groups[1].Value), stoppingToken);
+								string fileName = fileMatch.Groups[2].Value;
+								for (int i = 0; i < _retryCount; i++)
+									try
+									{
+										using var response = await client.GetAsync(new Uri(url, fileMatch.Groups[1].Value), stoppingToken);
+										if(!response.IsSuccessStatusCode)
+										{
+											break;
+										}
+
 										response.EnsureSuccessStatusCode();
 
 										using var content = response.Content;
 										using var fileStream = new FileStream($"{dayPathTmp}\\{fileName}", FileMode.CreateNew);
 
-										await content.CopyToAsync(fileStream);
+										await content.CopyToAsync(fileStream, stoppingToken);
 
-                                        files.Add(fileName);
-                                        _cache.Set(fileName, FileDownloadStatusType.Success);
+										files.Add(fileName);
+										_cache.Set(fileName, FileDownloadStatusType.Success);
 
-                                        break;
-                                    }
-                                    catch (Exception)
-                                    {
-                                        if (i == 10)
-                                            throw;
+										break;
+									}
+									catch (Exception)
+									{
+										if (i == _retryCount || stoppingToken.IsCancellationRequested)
+										{
+											throw;
+										}
 
-                                        await Task.Delay(_retryDelay, stoppingToken);
-                                        _logger.LogInformation("\t\tTry N:{int} loading {string}", i, fileName);
-                                    }
-                            }));
+										await Task.Delay(_retryDelay, stoppingToken);
+										_logger.LogInformation("\t\tTry N:{int} loading {string}", i, fileName);
+									}
+							}));
 
-							Directory.Move(dayPathTmp, dayPath);
+							if (files.Count() > 0)
+							{
+								Directory.Move(dayPathTmp, dayPath);
 
-							dirs.Add(day);
-							_cache.Set(day, files);
-							_logger.LogInformation("\tLoading success to {string}", dayPath);
+								dirs.Add(day);
+								_cache.Set(day, files.ToList());
+								_logger.LogInformation("\tLoading success to {string}", dayPath);
+							}
+							else
+							{
+                                _logger.LogInformation("\t\tFolder {string} not loaded.", day);
+                                Directory.Delete(dayPathTmp);
+                            }
 						}
 					}
-					else
-					{
-						_logger.LogInformation("\tNew data not found.");
-					}
-				}
+				}));
+				_logger.LogInformation("\tSleep...");
 
-				await Task.Delay(_config.HoursInterval * 24 * 3600000 - (int)(DateTime.Now.TimeOfDay.TotalMilliseconds - startTime.TimeOfDay.TotalMilliseconds), stoppingToken);
+                await Task.Delay(_config.HoursInterval * 24 * 3600000 - (int)(DateTime.Now.TimeOfDay.TotalMilliseconds - startTime.TimeOfDay.TotalMilliseconds), stoppingToken);
 			}
 		}
 	}
